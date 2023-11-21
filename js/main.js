86c08af91b53fd753d4835137588b67ef0216b76
vasishth chaudhari <you@example.com>
2023-11-21 16:00:57 -0500

    updating

--- js/main.js
+++ js/main.js
@@ -3,8 +3,14 @@
   const model = document.querySelector("#model");
   const hotspots = document.querySelectorAll(".Hotspot");
   const materialTemplate = document.querySelector("#material-template");
+  const materialHeading = document.querySelector("#material-heading")
   const materialList = document.querySelector("#material-list");
+  const spinner = `<svg id="custom-spinner" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M20.0001 12C20.0001 13.3811 19.6425 14.7386 18.9623 15.9405C18.282 17.1424 17.3022 18.1477 16.1182 18.8587C14.9341 19.5696 13.5862 19.9619 12.2056 19.9974C10.825 20.0328 9.45873 19.7103 8.23975 19.0612" stroke="#000000" stroke-width="3.55556" stroke-linecap="round"/>
+</svg>
 
+
+`;
   // Functions
   function modelLoaded() {
     hotspots.forEach(hotspot => {
@@ -41,9 +47,11 @@
 
 
   async function loadMaterialInfo() {
+    materialList.innerHTML = spinner;
     try {
       const response = await fetch('https://swiftpixel.com/earbud/api/materials');
       const materials = await response.json();
+      materialList.innerHTML = "";
 
       materials.forEach(material => {
         const clone = materialTemplate.content.cloneNode(true);
